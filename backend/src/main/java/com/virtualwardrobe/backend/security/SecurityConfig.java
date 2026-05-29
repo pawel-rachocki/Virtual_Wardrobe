5c18c1fca66b43173c318ad413fb80a09ba3cdbf
Pawel <pawel.logo@gmail.com>
2026-05-29 18:50:06 +0200

    CORS config

--- backend/src/main/java/com/virtualwardrobe/backend/security/SecurityConfig.java
+++ backend/src/main/java/com/virtualwardrobe/backend/security/SecurityConfig.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -53,7 +54,8 @@ public class SecurityConfig {
 
   @Bean
   public SecurityFilterChain getSecurityFilterChain(HttpSecurity http) throws Exception {
-    http.csrf(AbstractHttpConfigurer::disable)
+    http.cors(Customizer.withDefaults())
+        .csrf(AbstractHttpConfigurer::disable)
         .authorizeHttpRequests(
             auth -> auth.requestMatchers("/api/auth/**").permitAll().anyRequest().authenticated())
         .sessionManagement(
